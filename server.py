1e55600047d9d72b6a077ad65e1a64f86858e025
Steve Borba <sjborbajr@gmail.com>
2023-08-05 17:28:32 -0700

    Python working

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 import os
+import re
 from pymongo import MongoClient
-from flask import Flask, send_file, request
-from flask_socketio import SocketIO
+from flask import Flask, send_file, request, send_from_directory
+from flask_socketio import SocketIO, send, emit
 from urllib.parse import urlparse
 import datetime
 import base64
+import hashlib
 import random
 
-app = Flask(__name__, static_folder='public')
-socketio = SocketIO(app)
+app = Flask(__name__, static_folder='public', static_url_path='/')
+socketio = SocketIO(app, async_mode='eventlet')
 
 mongo_uri = os.environ.get('MONGODB', 'mongodb://localhost/?retryWrites=true')
 client = MongoClient(mongo_uri)
@@ -19,69 +21,79 @@ response_collection = database.allResponses
 
 @app.route('/')
 def index():
-    return send_file('public/index.html')
+  return send_file('public/index.html')
 
 @app.route('/client.js')
 def client_js():
-    return send_file('client.js', mimetype='text/javascript')
+  return send_file('client.js', mimetype='text/javascript')
+
+@app.route('/socket.io/socket.io.js')
+def socket_io_js():
+  return send_file('public/socket.io.js', mimetype='text/javascript')
+
+@socketio.on('saveChar')
+def handle_this(data):
+  print('Received event')
+  print(data)
 
 @socketio.on('connect')
-def handle_connect():
-    player_name = request.args.get('playerName', '').strip().replace('[^a-zA-Z0-9]', '')
-    client_ip = request.remote_addr
-    auth_nonce = request.args.get('authNonce', '')
+def handle_connect(sid):
+  player_name = sid.get('playerName', '')
+  player_name = re.sub('[^a-zA-Z0-9]', '', player_name)
 
-    print('[' + str(datetime.datetime.utcnow()) + '] User connected:', player_name, 'From:', client_ip)
+  client_ip = request.environ.get('REMOTE_ADDR')
+  auth_nonce = sid.get('authNonce', '')
 
-    player_data = fetch_player_data(player_name)
-    if player_data:
-        if player_data['name'] == player_name and player_data['authNonce'] == auth_nonce and auth_nonce != '':
-            pass
-            # Update database of new logon
-        elif player_data['authNonce'] != auth_nonce and player_data['name'] == player_name and client_ip in player_data['ipList']:
-            socketio.emit('nonce', player_data['authNonce'])
-        else:
-            socketio.emit("error", "user not authenticated")
-            socketio.disconnect()
-            print('Player', player_name, 'did not have nonce and did not have IP - Kicked')
+  print('[' + str(datetime.datetime.utcnow()) + '] User connected:', player_name, 'From:', client_ip)
+
+  player_data = fetch_player_data(player_name)
+  if player_data:
+    if player_data['name'] == player_name and player_data['authNonce'] == auth_nonce and auth_nonce != '':
+      pass
+      # Update database of new logon
+    elif player_data['authNonce'] != auth_nonce and player_data['name'] == player_name and client_ip in player_data['ipList']:
+      socketio.emit('nonce', player_data['authNonce'])
     else:
-        player_data = add_player(player_name, client_ip, socket)
-        if not player_data:
-            socketio.emit("error", 'Could not add user with name "' + player_name + '"')
-            socketio.disconnect()
-
-@socketio.on('message')
-def handle_message(data):
-    # Handle incoming messages
-    pass
+      socketio.emit("error", "user not authenticated")
+      socketio.disconnect()
+      print('Player', player_name, 'did not have nonce and did not have IP - Kicked')
+  else:
+    #player_data = add_player(player_name, client_ip, socket)
+    if not player_data:
+      socketio.emit("error", 'Could not add user with name "' + player_name + '"')
+      socketio.disconnect()
+    
+  print(player_data)
+  if player_data['admin'] == True:
+    socketio.emit('serverRole','admin')
 
 def fetch_player_data(player_name):
-    find_filter = {"name": player_name, "type": "player"}
-    player_data = None
-    try:
-        player_data = game_data_collection.find_one(find_filter)
-        return player_data
-    except Exception as error:
-        print(error)
-        raise error
+  find_filter = {"name": player_name, "type": "player"}
+  player_data = None
+  try:
+    player_data = game_data_collection.find_one(find_filter)
+    return player_data
+  except Exception as error:
+    print(error)
+    raise error
 
 def add_player(player_name, client_ip, socket):
-    if len(player_name) > 0:
-        print('Adding user:', player_name)
-        nonce = base64.b64encode(os.urandom(64)).decode('utf-8')
-        socketio.emit('nonce', nonce)
-        player_doc = {
-            "name": player_name,
-            "type": "player",
-            "ipList": [client_ip],
-            "authNonce": nonce
-        }
-        try:
-            game_data_collection.insert_one(player_doc)
-            return player_doc
-        except Exception as error:
-            print('Error saving response to MongoDB:', error)
-    return None
+  if len(player_name) > 0:
+    print('Adding user:', player_name)
+    nonce = base64.b64encode(os.urandom(64)).decode('utf-8')
+    socketio.emit('nonce', nonce)
+    player_doc = {
+      "name": player_name,
+      "type": "player",
+      "ipList": [client_ip],
+      "authNonce": nonce
+    }
+    try:
+      game_data_collection.insert_one(player_doc)
+      return player_doc
+    except Exception as error:
+      print('Error saving response to MongoDB:', error)
+      return None
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 9001)))
